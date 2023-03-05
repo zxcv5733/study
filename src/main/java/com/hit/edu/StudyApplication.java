496f8bc39fe28421b3cecdf9dea6b55022b2d2b2
zxcv5733 <15134576272@163.com>
2023-03-05 20:41:40 +0800

    Revert "异步线程池"
    This reverts commit 09eca676

--- src/main/java/com/hit/edu/StudyApplication.java
+++ src/main/java/com/hit/edu/StudyApplication.java
@@ -2,13 +2,11 @@ package com.hit.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @Author Ling dong.
  * @Date 2020/9/20 - 20:01
  */
-@EnableAsync
 @SpringBootApplication
 public class StudyApplication {
     public static void main(String[] args) {
